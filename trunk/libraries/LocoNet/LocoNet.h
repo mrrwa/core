3968d7728c2c3998a2d3c5b98e2ea7852db4cd4b
da2000 <da2000@476c6c09-0a78-4e69-82ba-c4d33c2a2adf>
2014-01-25 20:33:52 +0000

    Merged Comments from unifiedConfiguration branch into trunk Deleted unifiedConfiguration branch

--- trunk/libraries/LocoNet/LocoNet.h
+++ trunk/libraries/LocoNet/LocoNet.h
@@ -351,16 +351,99 @@ class LocoNetSystemVariableClass
     uint8_t DeferredProcessingRequired ;
     uint8_t DeferredSrcAddr ;
     
+	/** Read a value from the given EEPROM offset.
+	 *
+	 * There are two special values for the Offset parameter:
+	 *	SV_ADDR_EEPROM_SIZE - Return the size of the EEPROM
+	 *  SV_ADDR_SW_VERSION - Return the value of swVersion
+	 *  3 and on - Return the byte stored in the EEPROM at location (Offset - 2)
+	 *
+	 * Parameters:
+	 *		Offset: The offset into the EEPROM. Despite the value being passed as 2 Bytes, only the lower byte is respected.
+	 *
+	 * Returns:
+	 *		A Byte containing the EEPROM size, the software version or contents of the EEPROM.
+	 *
+	 */
     uint8_t readSVStorage(uint16_t Offset );
+	
+	/** Write the given value to the given Offset in EEPROM.
+	 *
+	 * TODO: Writes to Offset 0 and 1 will cause data corruption.
+	 *
+	 * Fires notifySVChanged(Offset), if the value actually chaned.
+	 *
+	 * Returns:
+	 *		A Byte containing the new EEPROM value (even if unchanged).
+	 */
     uint8_t writeSVStorage(uint16_t Offset, uint8_t Value);
+	
+	/** Checks whether the given Offset is a valid value.
+	 *
+	 * Returns:
+	 *		True - if the given Offset is valid. False Otherwise.
+	 */
     uint8_t isSVStorageValid(uint16_t Offset);
+	
+	/** Read the NodeId (Address) for SV programming of this module.
+	 *
+	 * This method accesses multiple special EEPROM locations.
+	 */
     uint16_t readSVNodeId(void);
+	
+	/** Write the NodeId (Address) for SV programming of this module.
+	 *
+	 * This method accesses multiple special EEPROM locations.
+	 */
     uint16_t writeSVNodeId(uint16_t newNodeId);
+	
+	/**
+	 * Checks whether all addresses of an address range are valid (defers to
+	 * isSVStorageValid()). Sends a notification for the first invalid address
+	 * (long Ack with a value of 42).
+	 *
+	 *	TODO: There is a Type error in this method. Return type is bool, but
+	 *		actual returned values are Integer.
+	 *
+	 * Returns:
+	 *		0 if at least one address of the range is not valid.
+	 *		1 if all addresses out of the range are valid.
+	 */
     bool CheckAddressRange(uint16_t startAddress, uint8_t Count);
 
   public:
 	void init(uint16_t newVendorId, uint16_t newDeviceId, uint8_t newSwVersion);
+	
+	/**
+	 * Check whether a message is an SV programming message. If so, the message
+	 * is processed.
+	 * Call this message in your main loop to implement SV programming.
+	 *
+	 * TODO: This method should be updated to reflect whether the message has
+	 *	been consumed.
+	 *
+	 * Note that this method will not send out replies.
+	 *
+	 * Returns:
+	 *		SV_OK - the message was or was not an SV programming message.
+				It may or may not have been consumed.
+	 *		SV_DEFERRED_PROCESSING_NEEDED - the message was an SV programming
+				message and has been consumed. doDeferredProcessing() must be
+				called to actually process the message.
+	 *		SV_ERROR - the message was an SV programming message and carried
+				an unsupported OPCODE.
+	 *
+	 */
 	SV_STATUS processMessage(lnMsg *LnPacket );
+	
+	/**
+	 * Attempts to send a reply to an SV programming message.
+	 * This method will repeatedly try to send the message, until it succeeds.
+	 *
+	 * Returns:
+	 *		SV_OK - Reply was successfully sent.
+	 *		SV_DEFERRED_PROCESSING_NEEDED - Reply was not sent, a later retry is needed.
+	 */
     SV_STATUS doDeferredProcessing( void );
 };
 
