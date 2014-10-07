ef1e54980d74d000cbb681d15f12c5ad84963251
kiwi64ajs <kiwi64ajs@476c6c09-0a78-4e69-82ba-c4d33c2a2adf>
2014-10-07 11:43:40 +0000

    Added support for Mobile Decoder Functions 13..28

--- trunk/libraries/NmraDcc/NmraDcc.h
+++ trunk/libraries/NmraDcc/NmraDcc.h
@@ -26,7 +26,7 @@
 #define NMRA_DCC_PROCESS_SERVICEMODE
 
 // Uncomment the following line to Enable MutliFunction Decoder Operations
-//#define NMRA_DCC_PROCESS_MULTIFUNCTION
+#define NMRA_DCC_PROCESS_MULTIFUNCTION
 
 #if defined(ARDUINO) && ARDUINO >= 100
 #include "Arduino.h"
@@ -83,6 +83,49 @@ typedef struct
 #define CV_29_CONFIG                          29
 #define MAXCV                                 E2END     // the upper limit of the CV value currently defined to max memory.
 
+typedef enum
+{
+	FN_0_4 = 1,
+	FN_5_8,
+	FN_9_12,
+	FN_13_20,
+	FN_21_28
+} FN_GROUP;
+
+#define FN_BIT_00	0x10
+#define FN_BIT_01	0x01
+#define FN_BIT_02	0x02
+#define FN_BIT_03	0x04
+#define FN_BIT_04	0x08
+
+#define FN_BIT_05	0x01
+#define FN_BIT_06	0x02
+#define FN_BIT_07	0x04
+#define FN_BIT_08	0x08
+
+#define FN_BIT_09	0x01
+#define FN_BIT_10	0x02
+#define FN_BIT_11	0x04
+#define FN_BIT_12	0x08
+
+#define FN_BIT_13	0x01
+#define FN_BIT_14	0x02
+#define FN_BIT_15	0x04
+#define FN_BIT_16	0x08
+#define FN_BIT_17	0x10
+#define FN_BIT_18	0x20
+#define FN_BIT_19	0x40
+#define FN_BIT_20	0x80
+
+#define FN_BIT_21	0x01
+#define FN_BIT_22	0x02
+#define FN_BIT_23	0x04
+#define FN_BIT_24	0x08
+#define FN_BIT_25	0x10
+#define FN_BIT_26	0x20
+#define FN_BIT_27	0x40
+#define FN_BIT_28	0x80
+
 class NmraDcc
 {
   private:
@@ -116,7 +159,7 @@ extern void notifyDccReset(uint8_t hardReset ) __attribute__ ((weak));
 extern void notifyDccIdle(void) __attribute__ ((weak));
 
 extern void notifyDccSpeed( uint16_t Addr, uint8_t Speed, uint8_t ForwardDir, uint8_t MaxSpeed ) __attribute__ ((weak));
-extern void notifyDccFunc( uint16_t Addr, uint8_t FuncNum, uint8_t FuncState) __attribute__ ((weak));
+extern void notifyDccFunc( uint16_t Addr, FN_GROUP FuncGrp, uint8_t FuncState) __attribute__ ((weak));
 
 extern void notifyDccAccState( uint16_t Addr, uint16_t BoardAddr, uint8_t OutputAddr, uint8_t State ) __attribute__ ((weak));
 
